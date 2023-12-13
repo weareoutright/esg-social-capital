00a535fd29a82a9fd4dd06dfb0ef26082b611dcd
Dakota Fabro <dakota@weareoutright.com>
2023-12-13 01:07:58 -0800

    Fix imports

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c43d971715d9af4a"; }
+/******/ 		__webpack_require__.h = function() { return "4cc678ddf1d58992"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/PhaseTitle.jsx
+++ components/PhaseTitle.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import Image from 'next/image';
 
+import Phase1HeaderContent from "./LayTheFoundation/Phase1HeaderContent"
+import Phase2HeaderContent from './DesignTheSupports/Phase2HeaderContent';
+import Phase3HeaderContent from "./ExecuteAndEvaluate/Phase3HeaderContent";
+
 const PhaseTitle = ({headerSvg, phaseNum, phaseTitle, phaseHeaderContent, phaseHeaderImg}) => {
+	const phaseHeaderContents = {
+		1: <Phase1HeaderContent />,
+		2: <Phase2HeaderContent />,
+		3: <Phase3HeaderContent />
+	  }
+
   return (
     <>
     <div className="sidebar">
@@ -16,7 +26,7 @@ const PhaseTitle = ({headerSvg, phaseNum, phaseTitle, phaseHeaderContent, phaseH
 					<h3>
 						{phaseTitle}
 					</h3>
-				{phaseHeaderContent}
+				{phaseHeaderContents[phaseHeaderContent]}
 				</div>
 				<div className="phase1-title-img">
 					<Image src={phaseHeaderImg} alt="" />
