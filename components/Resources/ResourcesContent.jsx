b6b69efeb4b8305286f59fb53933caf8d76a5aa9
Dakota Fabro <dakota@weareoutright.com>
2023-12-20 13:42:21 -0800

    Update acknowledgement

--- components/Resources/ResourcesContent.jsx
+++ components/Resources/ResourcesContent.jsx
@@ -10,7 +10,7 @@ const ResourcesContent = () => {
         <div className="resources-acknowledgement">
             <h4>Acknowledgements</h4>
           <p className="content-justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          <em>The development of this microsite was made possible through the generous support of the Bill and Melinda Gates Foundation and the Siemens Foundation.</em>
           </p>
         </div>
         <hr></hr>
