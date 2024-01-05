ecaa6e0bf994192ad08effa2fe4df0b8f7d26886
Dakota Fabro <dakota@weareoutright.com>
2024-01-05 15:01:13 -0800

    Implement share button copy feature

--- components/HeaderCustom.jsx
+++ components/HeaderCustom.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ESG_LOGO from "../constants/esg_logo.svg"
 import Image from 'next/image'
 import Link from 'next/link'
+import {CopyToClipboard} from "react-copy-to-clipboard"
 
 const HeaderCustom = ({homePageHeader}) => {
+  const [copied, setCopied] = useState(false)
+  const SHARE_LINK = "https://esg-social-capital.vercel.app/"
+
   return (
     <div className="HeaderCustom">
         <div className="logo">
@@ -11,8 +15,11 @@ const HeaderCustom = ({homePageHeader}) => {
         </div>
 
         <div className={`pill ${homePageHeader === false ? "non-home-header" : ""}`}>
-            <button>DOWNLOAD FRAMEWORK RESOURCES</button>
-            <button>Share</button>
+            <Link className="pill navPill-whiteBg" href="https://drive.google.com/file/d/1uuxbRu4V1TRfjpMrTurJ6tSoy9iYNRp_/view">DOWNLOAD FRAMEWORK RESOURCES</Link>
+            <CopyToClipboard text={SHARE_LINK}
+              onCopy={() => setCopied(true)}>
+              <button>Share</button>
+            </CopyToClipboard>
         </div>
     </div>
   )
