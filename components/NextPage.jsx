70b342530891f7e0200efca70c4b944050cc280f
Dakota Fabro <dakota@weareoutright.com>
2024-01-07 14:40:36 -0800

    Change next button in NextPage

--- components/NextPage.jsx
+++ components/NextPage.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
 import NEXT_ARROW from "../constants/nextArrow.svg"
+import NEXT_ARROW_DARK from "../constants/nextArrow-dark.svg"
 
 const NextPage = ({nextPage, nextPageAnchorLink}) => {
+  const [nextArrow, setNextArrow] = useState(NEXT_ARROW)
+
   return (
     <div className="NextPage">
       {nextPage !== null ?
       <>
       <span>Continue to {nextPage}</span>
-      <Link className="pill" href={nextPageAnchorLink} alt=""><span>Next</span> <Image src={NEXT_ARROW} alt="" /></Link>
+      <Link className="pill" href={nextPageAnchorLink} alt="" onMouseEnter={() => setNextArrow(NEXT_ARROW_DARK)} onMouseLeave={() => setNextArrow(NEXT_ARROW)}><span>Next</span> <Image src={nextArrow} alt="" /></Link>
       </> : <span></span>
       }
     </div>
