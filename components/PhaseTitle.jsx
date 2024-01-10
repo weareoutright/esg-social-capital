0e1b9a46c2035c50cee9b3396a574154989d0884
Dakota Fabro <dakota@weareoutright.com>
2024-01-10 11:54:13 -0800

    Add network graphic changes

--- components/PhaseTitle.jsx
+++ components/PhaseTitle.jsx
@@ -2,14 +2,46 @@ import React from 'react'
 import Image from 'next/image';
 
 const PhaseTitle = ({headerSvg, phaseNum, phaseTitle, phaseHeaderContent, phaseHeaderImg}) => {
+	console.log(phaseTitle)
+
+
+	const layTheFoundationNetworkStyles = {
+        height: "46vh",
+        top: "22%",
+        right: "0",
+		borderRadius: "50%",
+		boxShadow: "0 0 .5em 1em #3870B5 inset",
+	}
+
+	const designTheSupportsNetworkStyles = {
+        height: "46vh",
+        top: "22%",
+        right: "0",
+		borderRadius: "50%",
+		boxShadow: "0 0 .5em .15em #3870B5 inset",
+	}
+
+	const execAndEvalNetworkStyles = {
+		height: "100vh",
+		top: "0",
+		right: "0",
+		borderRadius: "0%",
+	}
+
   return (
     <>
-    <div className="sidebar">
+    <div className="sidebar" 
+			style={phaseTitle === "Lay The Foundation" ? layTheFoundationNetworkStyles : phaseTitle === "Design The Supports" ? designTheSupportsNetworkStyles : phaseTitle === "Execute & Evaluate" ? execAndEvalNetworkStyles : {}}
+	>
 				<span></span>
 	</div>
-	{/* <div className="sidebar-filter">
+	{/* <div className="sidebar-blur"
+	>
 				<span></span>
 	</div> */}
+	<div className="sidebar-filter">
+				<span></span>
+	</div>
 			<div className='title-div'>
 				<div>
 				<p className="phase-pin"><Image src={headerSvg} alt="" /><span>PHASE {phaseNum}</span></p>
--- components/Resources/ResourcesHeader.jsx
+++ components/Resources/ResourcesHeader.jsx
@@ -3,12 +3,24 @@ import Image from 'next/image'
 
 import RESOURCES_FOLDER from "../../constants/resources-folder.svg"
 
+const resourcesNetworkStyles = {
+	height: "100vh",
+	top: "0",
+	right: "0",
+	borderRadius: "0%",
+	backgroundSize: "13em",
+	boxShadow: "0 0 .5em .25em #3870B5 inset",
+}
+
 const ResourcesHeader = () => {
   return (
     <>
-    <div className="sidebar">
+    <div className="sidebar" style={resourcesNetworkStyles}>
 				<span></span>
 	  </div>
+	  <div className="sidebar-filter">
+				<span></span>
+	</div>
 			<div className='title-div'>
 				<div>
 				<p className="phase-pin"><Image src={RESOURCES_FOLDER} alt="" /><span className='resources-phase-title'>Career Connections</span></p>
