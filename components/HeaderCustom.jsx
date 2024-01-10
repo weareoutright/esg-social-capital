a51c665a65af9af12d3133e4e4c6c2043be49064
Dakota Fabro <dakota@weareoutright.com>
2024-01-10 12:34:34 -0800

    Add cultivation connections logo to header

--- components/HeaderCustom.jsx
+++ components/HeaderCustom.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react'
 import ESG_LOGO from "../constants/esg_logo.svg"
+import CULTIVATING_CONNECTIONS from "../constants/cultivating-connections-header.svg"
 import Image from 'next/image'
 import Link from 'next/link'
-import {CopyToClipboard} from "react-copy-to-clipboard"
+// import {CopyToClipboard} from "react-copy-to-clipboard"
 
 const HeaderCustom = ({homePageHeader}) => {
   // const [copied, setCopied] = useState(false)
@@ -11,6 +12,7 @@ const HeaderCustom = ({homePageHeader}) => {
   return (
     <div className="HeaderCustom">
         <div className="logo">
+            <Link href="/" target="_blank" rel="noreferrer" className="logo-link"><Image src={CULTIVATING_CONNECTIONS} alt="ESG LOGO"/></Link>
             <Link href="https://edstrategy.org/" target="_blank" rel="noreferrer" className="logo-link"><Image src={ESG_LOGO} alt="ESG LOGO"/></Link>
         </div>
 
