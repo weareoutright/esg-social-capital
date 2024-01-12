d3f6b8a98266a791bd6434e2dd2b1912f3bd3b5d
Dakota Fabro <dakota@weareoutright.com>
2024-01-12 15:21:12 -0800

    Adjust content for hero

--- components/HeaderCustom.jsx
+++ components/HeaderCustom.jsx
@@ -4,6 +4,7 @@ import React, {useState} from 'react'
 import ESG_LOGO from "../constants/esg_logo.svg"
 import CULTIVATING_CONNECTIONS from "../constants/cultivating-connections-header.svg"
 import HAMBURGER_MENU from "../constants/hamburger-menu-icon.svg"
+import CLOSE_MENU from "../constants/close-menu-icon.svg"
 import Image from 'next/image'
 import Link from 'next/link'
 import MobileMenu from "./MobileMenu"
@@ -16,8 +17,10 @@ const HeaderCustom = ({homePageHeader}) => {
     <div className="HeaderCustom">
         <div className="logo">
             <Link href="/" className="logo-link"><Image src={CULTIVATING_CONNECTIONS} alt="ESG LOGO"/></Link>
-            <button onClick={() => setShowMenu(!showMenu)} className="hamburger-menu">Menu 
-            <Image src={HAMBURGER_MENU} alt="" className="hamburger-menu-icon"/>
+            <button onClick={() => setShowMenu(!showMenu)} className="hamburger-menu">{showMenu === false ? "Menu" : "Close"} 
+
+            {showMenu === false ? <Image src={HAMBURGER_MENU} alt="" className="hamburger-menu-icon"/> : <Image src={CLOSE_MENU} alt="" className="hamburger-menu-icon"/>}
+            
             </button>
             <Link href="https://edstrategy.org/" target="_blank" rel="noreferrer" className="logo-link"><Image src={ESG_LOGO} alt="ESG LOGO"/></Link>
         </div>
