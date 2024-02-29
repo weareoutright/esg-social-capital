92a3ed90606b7c1f26d53c5732e218868fcccb61
Dakota Fabro <dakota@weareoutright.com>
2024-02-29 10:27:14 -0800

    Parse markup

--- components/ActionStep.jsx
+++ components/ActionStep.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Image from 'next/image'
 import Tool from './Tool'
 import StickyNote from './StickyNote'
+import parse from 'html-react-parser'
 
 const ActionStep = ({icon, title, description, toolsArr, stickyNotesArr}) => {
+    let descriptionContent = parse(description)
   return (
     <div className="ActionStep">
         <div className="action-step-title">
@@ -11,13 +13,12 @@ const ActionStep = ({icon, title, description, toolsArr, stickyNotesArr}) => {
             <h5 className='action-title'>{title}</h5>
         </div>
         <p className="content-justify">
-            {description}
+            {descriptionContent}
         </p>
 
         <div className="tools">
         <span>Tools</span>
         {toolsArr.map((tool, index) => {
-            console.log(tool)
             return <Tool key={index} href={tool.link.url} title={tool.link.title} />
         })}
         </div>
@@ -26,12 +27,13 @@ const ActionStep = ({icon, title, description, toolsArr, stickyNotesArr}) => {
             <div className="stickyNotes">
             <span>Notes from the field</span>
                 {stickyNotesArr.map((note, index) => {
+                    const description = parse(note.description)
                 return (
                     <StickyNote 
                     key={index} 
                     title={note.title} 
                     location={note.location} 
-                    content={note.description} 
+                    content={description} 
                     learnMoreHref={note.callToAction.url} />
                 )})}
             </div> : 
